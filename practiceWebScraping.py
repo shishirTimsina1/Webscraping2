f98f03631629aaddd3e4b08423ec599c3bc94d45
sht99 <44593416+sht99@users.noreply.github.com>
2022-06-25 13:54:56 -0400

    added url

--- practiceWebScraping.py
+++ practiceWebScraping.py
@@ -2,12 +2,12 @@ import re
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
-myUrl2 = "https://www.newegg.com/p/pl?N=100007709%2050001315"
-myUrl = "https://www.newegg.com/Desktop-Graphics-Cards/SubCategory/ID-48?Tid=7709"
+myUrl2 = "https://www.newegg.com/p/pl?Submit=StoreIM&Category=38&Depa=1"
+myUrl = "https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38"
 myUrl3 = "https://www.newegg.com/p/pl?d=monitors"
 # opening connection, grabbing page
 # use the two options above, or try any newegg webpage that supports functions used below.
-uClient = uReq(myUrl)
+uClient = uReq(myUrl2)
 page_html = uClient.read()
 uClient.close()
 
@@ -23,15 +23,18 @@ filename = "GraphicsCards.csv"
 # use own file name, and eliminate the headers write funtion.
 f = open(filename, "w")
 
-headers = "Brand, Product Name, Price, Shipping \n"
+headers = "Brand, Product Name, Price, Shipping, URL \n"
 f.write(headers)
 
 # grab each product
 containers = page_soup.findAll("div", {"class": "item-container"} )
 for container in containers:
-    brand = container.div.div.a.img["title"]
     titleContainer = container.findAll("a", {"class": "item-title"})
     productName = titleContainer[0].text
+    brand = productName[0:3]
+    url = container.find("a")
+    url = url.get('href')
+    url = str(url)
     priceCont = container.findAll("li", {"class": "price-current"})
     price = priceCont[0].text.strip()
     if price.find('('):
@@ -43,13 +46,12 @@ for container in containers:
 
     # write out to file
 
-    f.write(brand + "," + productName.replace(',','|') + "," + finalPrice.replace(',', '') + "," + shipping + "\n")
+    f.write(brand + "," + productName.replace(',','|') + "," + finalPrice.replace(',', '') + "," + shipping + "," + url +"\n")
 
 f.close()
     #print("\nBrand:"+ brand)
     #print("Product Name:"+ productName)
     #print("Price: " + price)
     #print("Shipping:"+ shipping + "\n")
-
 
 
